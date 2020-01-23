f67b360034812bfce07ddf49f6a94d53db1a2af8
SzMariann <mariann.szabo.90@gmail.com>
2020-01-23 17:08:44 +0100

    final touches

---
+++ BPPB.js
@@ -0,0 +1,12 @@
+x = document.getElementById(x);
+
+function fadeIn(x){
+    x.style.opacity = 0.3;
+    x.style.transitionDuration = "0.5s";
+}
+
+
+function fadeOut(x){
+    x.style.opacity = 1;
+    x.style.transitionDuration = "0.5s";    
+}
