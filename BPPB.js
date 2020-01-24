404251732b689fea391ed68e49a04af3bdc438cf
Tamara Bonyai <tamamara18@gmail.com>
2020-01-24 14:35:14 +0100

    Prices and Reviews page

--- BPPB.js
+++ BPPB.js
@@ -1,4 +1,4 @@
-x = document.getElementById(x);
+let x = document.getElementById('x');
 
 function fadeIn(x){
     x.style.opacity = 0.3;
@@ -10,3 +10,27 @@ function fadeOut(x){
     x.style.opacity = 1;
     x.style.transitionDuration = "0.5s";    
 }
+
+console.log("repa")
+
+$('#modalForm').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('.modal-title').text('Chosen: ' + recipient)
+  })
+
+
+$(document).ready(function(){
+    var date_input=$('input[name="date"]'); //our date input has the name "date"
+    var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+    var options={
+      format: 'mm/dd/yyyy',
+      container: container,
+      todayHighlight: true,
+      autoclose: true,
+    };
+    date_input.datepicker(options);
+  })
